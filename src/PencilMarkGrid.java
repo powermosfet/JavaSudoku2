cf6987346c52e33a84cffa177e0ea048ea29b652
Asmund <asmundliseth@gmail.com>
2012-12-21 22:54:00 +0100

    Added a bare PencilMarkGrid class
    A minimal test class was added, with one failing test

---
+++ src/PencilMarkGrid.java
@@ -0,0 +1,13 @@
+package com.liseth.javaSudoku2;
+
+class PencilMarkGrid
+{
+	public PencilMarkGrid(char[] tokens){
+	}
+
+	private char[][][] _pencilMarks;
+
+	public int GetSize(){
+		return 0;
+	}
+}
