b88735af57c6f4f2e7890b5365a846ed4a77e5ac
Asmund <asmundliseth@gmail.com>
2012-12-31 10:20:11 +0100

    Added test for CellArray.contains(Cell)

---
+++ tests/CellArrayTest.java
@@ -0,0 +1,24 @@
+package com.liseth.javaSudoku2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+/**
+* Tests for {@link CellArray}.
+*
+* @author asmundliseth@gmail.com (Asmund Liseth)
+*/
+@RunWith(JUnit4.class)
+public class CellArrayTest {
+	@Test
+	public void contains(){
+		CellGrid g = Std.grid();
+		CellArray column = g.columns().get(3);
+		assertTrue(column.contains(g.get(5, 3)));
+		assertFalse(column.contains(g.get(3, 2)));
+	}
+}
